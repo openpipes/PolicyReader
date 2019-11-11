05bd060db12d3856517aa6b169e275e835c4a58e
Yi Wu <WYmario@163.com>
2019-11-11 17:19:08 +0800

    Add sub-classes of scripts
    Arrange the framework by several blocks.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # policy-reader
 Policy reader is a handy toolkit for users to easily understand the components of a regulatory document, policy or legislation file etc. 
+
+
+
+### Demonstration
+
---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+		Policy Reader Toolkit (v1.0)
+This toolkit contains a list of components helping with parse context and extract useful entities in a Policy or regulatory document.
+
+
+
+"""
+from .planning import Planning
+from .db import Database
+from .parser import Parser
+from .nlp import NLP
+from .time import Time
